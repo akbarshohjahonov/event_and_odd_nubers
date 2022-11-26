44b60af7444776cf14ff87e3b4cf8120bba2e75a
akbarshohjahonov <akbarshohjahonov@akbarshohjahonov@gmail.com>
2022-11-26 15:57:06 +0500

    olla

--- sum_odd.py
+++ sum_odd.py
@@ -1,10 +1,12 @@
 #A four-digit integer is given. Find the sum of odd digits in it.
-t=2797
+var_int=2797
+sum_even=0
 #Create a variable "var_int" and assign it a four-digit integer value
 #Create a variable "sum_even" and assign it 0.
-a=t%10
-b=t//1000
-c=(t//100)%10
-e=((t//10)%10)
+a=var_int%10
+b=var_int//1000
+c=(var_int//100)%10
+e=((var_int//10)%10)
 print((a%2)*a+(b%2)*b+(c%2)*c+(e%2)*e)
+print(sum_even)
 #Find the sum of the odd digits in the variable "var_int".
